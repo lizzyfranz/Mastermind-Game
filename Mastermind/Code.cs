fa32bcd01d519537f5c0c08e19b9f3513083e39a
lizzyfranz <44844041+lizzyfranz@users.noreply.github.com>
2020-10-13 13:57:21 -0400

    completed game

---
+++ Mastermind/Code.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mastermind
+{
+    class Code
+    {
+        private int[] code;
+
+        //fill a 4 digit code with integers between 1 and 6
+        public Code()
+        {
+            System.Random rand = new System.Random();
+            code = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                code[i] = rand.Next(1, 7);
+            }
+        }
+
+        //checks the guess against the code
+        //returns a string with + for every correct guess and - for every
+        //  incorrectly placed guess
+        public String checkGuess(int[] guess)
+        {
+            String printOut = "";
+            //refers to a number in the guess being connected to a code number
+            Boolean[] guessUsed = { false, false, false, false };
+            //refers to a location in the code being accounted for in the printout
+            Boolean[] locationMarked = { false, false, false, false };
+
+            //loop through the guess to check if any are exactly correct
+            for (int i = 0; i < 4; i++)
+            {
+                //print + if the digit is exactly correct
+                if (guess[i] == code[i])
+                {
+                    printOut += "+";
+                    guessUsed[i] = true;
+                    locationMarked[i] = true;
+                }
+            }
+
+            //loop through the guess to check if any are correct in the wrong spot
+            for (int guessLocation = 0; guessLocation < 4; guessLocation++)
+            {
+                int codeLocation = 0;
+                //if the guess has not been used then loop through the code until you find a match or get to the end
+                while (guessUsed[guessLocation] == false && codeLocation < 4)
+                {
+                    //if that code number has not been marked and the guess is right
+                    if (!locationMarked[codeLocation] && (guess[guessLocation] == code[codeLocation]))
+                    {
+                        guessUsed[guessLocation] = true;
+                        locationMarked[codeLocation] = true;
+                        printOut += "-";
+                    }
+
+                    codeLocation++;
+                }
+            }
+
+            //return the string
+            return (printOut);
+        }
+
+        public String getCode()
+        {
+            String c = "";
+            for (int i = 0; i < 4; i++)
+            {
+                c += this.code[i];
+            }
+
+            return c;
+        }
+    }
+    
+}
--- Mastermind/Program.cs
+++ Mastermind/Program.cs
@@ -1,12 +1,109 @@
 ﻿using System;
+using System.Runtime.ConstrainedExecution;
 
 namespace Mastermind
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create code for this game
+            Code code = new Code();
+            //boolean to indicate if the player has guessed correctly and won
+            Boolean won = false;
+            //guess number that the player is on
+            int guessNum = 0;
+
+            Console.WriteLine("Hello and welcome to Mastermind, you have 10 guesses. Good luck!\n");
+
+            //game loop of 10 guesses
+            while (!won && (guessNum < 10))
+            {
+                String readIn = "";
+                //keep letting the user guess until they guess in a valid format
+                do
+                {
+                    Console.Write("Guess " + (guessNum + 1) + ": ");
+                    readIn = Console.ReadLine();
+                } while (!goodInput(readIn));
+                
+                //format the user's input for easier evaluation
+                int[] guess = formatInput(readIn);
+
+                String hint = code.checkGuess(guess);
+                if (hint.Equals("++++"))
+                {
+                    won = true;
+                } else
+                {
+                    Console.WriteLine(hint);
+                    Console.WriteLine();
+                }
+
+                guessNum++;
+            } //end of guessing loop
+
+            //finish the game based on whether the player won or not
+            if (won)
+            {
+                Console.WriteLine("\nCongratulations! You beat Mastermind!");
+            }
+            else
+            {
+                Console.WriteLine("\nSorry, you've run out of guesses. The code was " + code.getCode() + ". Better luck next time!");
+            }
+        }
+
+        //checks the user input to make sure it is a number with four digits
+        //all four digits should be between 1-6
+        //inputs true for a valid input and false otherwise
+        //prints an error message if applicable when called
+        public static Boolean goodInput(string guess)
+        {
+            int g; 
+            if (!int.TryParse(guess, out g))
+            {
+                Console.WriteLine("Your input must be a number\n");
+                return false;
+            } else
+            {
+                //check for 4 digits
+                if (guess.Length != 4)
+                {
+                    Console.WriteLine("Your input must be four digits long\n");
+                    return false;
+                }
+
+                int[] guessArray = formatInput(guess);
+
+                //check that all of the digits are between 1 and 6
+                for (int i=0; i<4; i++)
+                {
+                    if (guessArray[i] < 1 || guessArray[i] > 6)
+                    {
+                        Console.WriteLine("Digits must be between 1 and 6\n");
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        //takes in a user input as a string and makes it an array for easier use
+        public static int[] formatInput(string guess)
+        {
+            int[] guessArray = new int[guess.Length];
+            int g = Convert.ToInt32(guess);
+            
+            for (int i=guess.Length - 1; i >= 0; i--)
+            {
+                guessArray[i] = g % 10;
+                g /= 10;
+            }
+            
+            return guessArray;
         }
     }
 }
